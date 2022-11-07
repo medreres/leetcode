4b842618badb6779d3ce5a9aa9252535be6bda38
medreres <90519417+medreres@users.noreply.github.com>
2022-11-07 22:50:29 +0200

    hash map, map, splice

--- 1365. How Many Numbers Are Smaller Than the Current Number/main.js
+++ 1365. How Many Numbers Are Smaller Than the Current Number/main.js
@@ -2,20 +2,38 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
 // hash map
  var smallerNumbersThanCurrent = function(nums) {
-    let map = {};
-    let copy = [...nums];
-    copy.sort()
+    let map = new Map();
+    let copy = Array.from(nums)
+    copy.sort((a,b) => (a>b)? 1: -1)
     
     copy.forEach((num,i) => {
-        if (!map[num])
-            map[num] = i;
+        if (map.get(num) === undefined)
+            map.set(num,i);
     })
     
     for (let i = 0; i < nums.length; i++) {
-        copy[i] = map[nums[i]];
+        copy[i] = map.get(nums[i]);
     }
     return copy;
+    
+};
+
+
+// elegant way
+var smallerNumbersThanCurrent = function(nums) {
+    let frequency = new Array(101).fill(0);
+    let smaller = new Array(101).fill(0);
+    
+    nums.forEach(num => frequency[num]++);
+    
+    for (let i = 1; i < 101; i++) {
+        smaller[i] = smaller[i-1] + frequency[i-1];
+    }
+    
+    nums.forEach((num, index) => nums[index] = smaller[nums[index]]);
+    return nums;
     
 };
